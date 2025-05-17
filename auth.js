17b54a4ba0333955fc5f19d2e4c408f7fa4464ed
ken <yash20004x17@gmail.com>
2025-05-17 21:30:11 +0530

    Working on second part of the course selling app backend

--- auth.js
+++ auth.js
@@ -1,5 +1,12 @@
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_SECRET = "kenx2004";
+
 // User Authentication
 function userAuth(req, res, next) {}
 
 // Admin Authentication
 function adminAuth(req, res, next) {}
+
+module.exports = {
+  JWT_ADMIN_SECRET,
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,94 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
 
-adminRouter.post("/signup", function (req, res) {});
+const { JWT_ADMIN_SECRET } = require("../auth.js");
 
-adminRouter.post("/login", function (req, res) {});
+// SignUp Schema
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  firstName: z.string().min(3),
+  lastName: z.string().min(2),
+});
+
+// LogIn Schema
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+// SignIn Route
+adminRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body;
+  const parsed = signupSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    res.status(400).json({
+      message: "Incorrect Data Format.",
+    });
+    return;
+  }
+
+  const existingUser = await adminModel.findOne({
+    email,
+  });
+  if (existingUser) {
+    res.json({
+      message: "Email already in use.",
+    });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await adminModel.create({
+    email,
+    password: hashedPassword,
+    firstName,
+    lastName,
+  });
+  res.json({
+    message: "Signed in Successfully.",
+  });
+});
+
+// Login Route
+adminRouter.post("/login", async function (req, res) {
+  const parsed = loginSchema.safeParse(req.body);
+  if (!parsed.success) {
+    res.json({
+      message: "Invalid Data Format.",
+    });
+    return;
+  }
+  const { email, password } = req.body;
+
+  const user = await adminModel.findOne({ email });
+
+  if (!user) {
+    res.json({
+      message: "User not found.",
+    });
+    return;
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if ((isValidPassword, user)) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_ADMIN_SECRET
+    );
+    res.json({
+      message: "Login Succesfully",
+      token: token,
+    });
+  }
+});
 
 adminRouter.post("/create-course", function (req, res) {});
 
